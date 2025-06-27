650718601734bda3085142037ea57dd0857a7948
Rafael Tonholo <rafael@tonholo.dev>
2025-06-26 21:48:52 -0300

    fix: background color

--- site/src/jsMain/kotlin/com/amandabicalho/portfolio/ui/theme/Color.kt
+++ site/src/jsMain/kotlin/com/amandabicalho/portfolio/ui/theme/Color.kt
@@ -12,7 +12,7 @@ internal val LightColorScheme = ColorScheme(
         shade50 = Color.argb(0xFF882D09),
     ),
     text = Color.argb(0xFF1B0902),
-    background = Color.argb(0xFFE5E4E4),
+    background = Color.argb(0xFFFFFCF7),
     gray = Color.argb(0xFFE5E4E4),
     white = Color.argb(0xFFE5E4E4),
 )
